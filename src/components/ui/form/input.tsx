b8a8e6e7542f441822b3f96c0754919380c7b478
Nathan Rodrigues Vieira <nathan.vieira@al.infnet.edu.br>
2025-09-18 12:41:05 -0300

    feat(form): add React Hook Form to form-input

--- src/components/ui/form/input.tsx
+++ src/components/ui/form/input.tsx
@@ -1,17 +1,25 @@
 import type { ComponentProps } from "react";
+import { useFormContext } from "react-hook-form";
 
 interface InputProps extends ComponentProps<"input"> {
   name: string;
   placeholder: string;
 }
 
-/*
-TODO -> Adicionar React Hook Form 
-*/
 export const Input = ({
   name = "",
   placeholder = "",
   ...props
 }: InputProps) => {
-  return <input className=" pt-2" id={name} placeholder={placeholder} {...props} />;
+  const { register } = useFormContext();
+
+  return (
+    <input
+      className=" pt-2"
+      id={name}
+      placeholder={placeholder}
+      {...props}
+      {...register(name)}
+    />
+  );
 };

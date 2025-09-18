80977679eb61b372635ea742861a7a362dfc8ef6
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-09-17 21:34:56 -0300

    feat(components): add add UserIcon component

---
+++ src/components/ui/user-icon/user-icon.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export const UserIcon = () => {
+  return (
+    <div className="p-4 bg-gradient-to-b from-zinc-200/50 via-stone-50/0 to-zinc-200/0 rounded-[96px] gap-4 items-start justify-start  outline-1 outline-offset-[-1px] outline-zinc-200">
+      <Image
+        src="/custom_icon.svg"
+        alt="Ícone de usuário"
+        width={32}
+        height={32}
+      />
+    </div>
+  );
+};

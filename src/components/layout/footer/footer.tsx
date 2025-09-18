2433de645bd0d54bd0bbb3e538e90e8397af3450
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-09-17 21:36:30 -0300

    feat(components): add Footer component

---
+++ src/components/layout/footer/footer.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Link from "next/link";
+
+export const Footer = () => {
+  return (
+    <footer className="pt-10 gap-6 flex flex-col justify-start items-center">
+      <div className=" inline-flex items-center justify-start gap-6">
+        {" "}
+        <Link
+          href="#"
+          className="text-sm font-semibold  leading-tight text-main"
+        >
+          {" "}
+          <span>Políticas de privacidade</span>
+        </Link>
+        <Link
+          href="#"
+          className="text-sm font-semibold leading-tight text-main"
+        >
+          <span>Termos de uso</span>
+        </Link>
+      </div>
+      <span className="text-sm font-semibold text-soft">
+        Teste de estágio {new Date().getFullYear()}
+      </span>
+    </footer>
+  );
+};

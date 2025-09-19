5e1faa95d47c7967b5e35948bac40e189725e417
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-09-18 21:19:20 -0300

    style(button): update Button component styles

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -11,8 +11,14 @@ export const Button = ({
   ...props
 }: ButtonProps) => {
   return (
-    <button type={type} {...props} className="cursor-pointer">
-      {children}
-    </button>
+    <div className="w-full z-20  pt-6 flex items-center justify-center">
+      <button
+        type={type}
+        {...props}
+        className="cursor-pointer h-12 w-full  px-6 bg-primary rounded-md justify-center items-center gap- hover:bg-secondary disabled:cursor-not-allowed text-white disabled:bg-primary disabled:opacity-40"
+      >
+        {children}
+      </button>
+    </div>
   );
 };

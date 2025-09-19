7d7a1666992bbfcb8051809fdc70fe26d811968d
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-09-18 21:17:43 -0300

    style(form): update container component styles

--- src/components/ui/form/container.tsx
+++ src/components/ui/form/container.tsx
@@ -1,3 +1,7 @@
 export const Container = ({ children }: { children: React.ReactNode }) => {
-  return <div className="w-96 gap-2 flex flex-col ">{children}</div>;
+  return (
+    <div className=" gap-2 items-center justify-center flex flex-col ">
+      {children}
+    </div>
+  );
 };

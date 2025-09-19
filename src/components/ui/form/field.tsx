2b6a82e4d441b1e89e710ed0d471400a8edb3b58
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-09-18 21:15:39 -0300

    feat(form): update field component styles to use tw-merge

--- src/components/ui/form/field.tsx
+++ src/components/ui/form/field.tsx
@@ -1,3 +1,15 @@
-export const Field = ({ children }: { children: React.ReactNode }) => {
-  return <div className="flex flex-col">{children}</div>;
+import type { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface FieldProps extends ComponentProps<"div"> {
+  children: React.ReactNode;
+  classname?: string;
+}
+
+export const Field = ({ children, className, ...props }: FieldProps) => {
+  return (
+    <div {...props} className={twMerge("flex flex-col ", className)}>
+      {children}
+    </div>
+  );
 };

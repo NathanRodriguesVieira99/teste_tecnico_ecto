9660ea167c9250718a848b1e3d00c3b1b739eb6a
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-09-18 23:11:10 -0300

    feat(ui): implement ShowPasswordButton component with visibility toggle logic

--- src/components/ui/show-password-button/show-password-button.tsx
+++ src/components/ui/show-password-button/show-password-button.tsx
@@ -1,12 +1,21 @@
-export const ShowPasswordButton = () => {
-  // TODO -> adicionar lógica de mostrar/ocultar senha
+type ShowPasswordButtonProps = {
+  isVisible: boolean; // armazena o valor do estado da senha (true(visível) ou false(oculto))
+  onClick: () => void; // função que altera o estado de visibilidade
+};
+
+export const ShowPasswordButton = ({
+  isVisible,
+  onClick,
+}: ShowPasswordButtonProps) => {
   return (
     <div className="flex items-center justify-center">
       <button
+        aria-label={isVisible ? "Mostrar" : "Esconder"}
         className="flex h-10 w-20 cursor-pointer items-center justify-center gap-2 overflow-hidden rounded-tr-md rounded-br-md border-soft border-t border-r border-b bg-white p-2"
+        onClick={onClick}
         type="button"
       >
-        Mostrar
+        {isVisible ? "Mostrar" : "Esconder"}
       </button>
     </div>
   );

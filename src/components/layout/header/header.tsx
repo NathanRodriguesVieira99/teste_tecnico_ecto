99b7f9e1f12ae7a35e790768bffdb05af2e22b15
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-09-18 21:21:21 -0300

    style(header):  update Header components styles

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -2,13 +2,15 @@ import Image from "next/image";
 
 export const Header = () => {
   return (
-    <header className="flex justify-between items-center pb-10 p-6">
-      <Image
-        src="/Logotipos.svg"
-        alt="Logotipo da Ecto"
-        width={96}
-        height={28}
-      />
+    <header className="flex w-96 relative justify-between h-5 pt-6 items-center p-6">
+      <div className="flex items-center justify-center">
+        <Image
+          src="/Logotipos.svg"
+          alt="Logotipo da Ecto"
+          width={96}
+          height={28}
+        />
+      </div>
       <span className="text-sm font-semibold leading-tight">
         Falar com um especialista
       </span>

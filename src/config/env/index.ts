20b15b86959708455e730afb8a59036321278020
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-09-19 21:04:31 -0300

    feat(env): add environment variable validation schema using zod

---
+++ src/config/env/index.ts
@@ -0,0 +1,23 @@
+import z from "zod";
+
+export const envSchema = z.object({
+  NEXT_PUBLIC_API_URL: z.url().startsWith("https://flow.ecto.tools"),
+  NEXT_PUBLIC_API_TOKEN: z.string(),
+  NODE_ENV: z.enum(["development", "test"]).default("development"),
+});
+
+const _env = envSchema.safeParse({
+  NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  NEXT_PUBLIC_API_TOKEN: process.env.NEXT_PUBLIC_API_TOKEN,
+  NODE_ENV: process.env.NODE_ENV,
+});
+
+if (_env.success === false) {
+  if (process.env.NODE_ENV === "development") {
+    // biome-ignore lint/suspicious/noConsole: log apenas no ambiente de dev
+    console.error(_env.error);
+  }
+  throw new Error("❌ Invalid environment variables ", _env.error);
+}
+
+export const env = _env.data;

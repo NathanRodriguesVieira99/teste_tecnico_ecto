3bef01696f3b7ff3bc4b684da613e07d93e94643
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-09-19 21:03:52 -0300

    feat(sign-up): integrate SignUpService with useSignUpPageModel for form submission

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,9 +1,16 @@
 "use client";
 
+import { HttpClient } from "@/infra/http/http-client";
+import { SignUpService as CreateAccountService } from "@/services/sign-up";
 import { useSignUpPageModel } from "./sign-up.model";
 import { SignUpPageView } from "./sign-up.view";
 
 export default function SignUpPage() {
-  const methods = useSignUpPageModel();
+  const http = HttpClient.create();
+
+  const SignUpService = new CreateAccountService(http);
+
+  const methods = useSignUpPageModel({ SignUpService });
+
   return <SignUpPageView {...methods} />;
 }
--- src/app/(auth)/sign-up/sign-up.model.ts
+++ src/app/(auth)/sign-up/sign-up.model.ts
@@ -1,10 +1,13 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
+import type { SignUpServiceBody } from "@/services/sign-up";
 import { type SignUpSchema, signUpSchema } from "./sign-up.schema";
+import type { SignUpServiceProps } from "./sign-up.types";
 
-export const useSignUpPageModel = () => {
+export const useSignUpPageModel = ({ SignUpService }: SignUpServiceProps) => {
   const methods = useForm<SignUpSchema>({
     resolver: zodResolver(signUpSchema),
   });
@@ -13,12 +16,27 @@ export const useSignUpPageModel = () => {
     formState: { isSubmitting, errors },
   } = methods;
 
+  const { mutateAsync: signUpServiceFn } = useMutation({
+    mutationFn: (data: SignUpServiceBody) => SignUpService.exec(data),
+  });
+
   const handleSignUp = async (data: SignUpSchema) => {
-    // TODO -> implementar lógica de SignUp
-    const TIMEOUT_SECONDS = 2000;
-    await new Promise((resolver) => setTimeout(resolver, TIMEOUT_SECONDS));
-    // biome-ignore lint/suspicious/noConsole: é temporário
-    console.log(data);
+    try {
+      await signUpServiceFn({
+        name: data.name,
+        last_name: data.last_name,
+        email: data.email,
+        phone: data.phone,
+        password: data.password,
+        confirm_password: data.confirm_password,
+      });
+
+      // biome-ignore lint/suspicious/noConsole: apenas para exibir o retorno.
+      console.log("Dados enviados: ", data);
+    } catch {
+      // biome-ignore lint/suspicious/noConsole: apenas para ter algo no catch.
+      console.error("Erro ao se cadastrar!");
+    }
 
     // limpa o input após o cadastro
     methods.reset();

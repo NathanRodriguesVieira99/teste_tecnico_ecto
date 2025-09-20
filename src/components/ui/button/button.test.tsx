bd15a1a52c34bd22f09f1577371d8e5c99deacdb
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-09-20 10:28:57 -0300

    test(button): add test when button is disabled

--- src/components/ui/button/button.test.tsx
+++ src/components/ui/button/button.test.tsx
@@ -2,11 +2,26 @@ import { render, screen } from "@testing-library/react";
 import { Button } from "./button";
 
 describe("<Button/>", () => {
-  it("Should", () => {
+  it("should render button component text", () => {
     render(<Button type="button">Cadastrar conta</Button>);
 
     const buttonText = screen.getByText("Cadastrar conta");
 
     expect(buttonText).toBeInTheDocument();
+  });
+  it("should show text when disabled", () => {
+    render(
+      <Button disabled={true} type="submit">
+        Cadastrando...
+      </Button>
+    );
+
+    const disabledButton = screen.getByRole("button", {
+      name: "Cadastrando...",
+    });
+
+    expect(disabledButton).toHaveProperty("disabled");
+    expect(disabledButton).toHaveTextContent("Cadastrando...");
+    expect(disabledButton).toBeDisabled();
   });
 });

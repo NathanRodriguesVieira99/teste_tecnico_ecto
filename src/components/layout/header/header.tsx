af1b5c270247188a662a83e83d4397b62b398586
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-09-21 16:56:21 -0300

    style(header): add responsive design for desktop screens

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export const Header = () => {
   return (
-    <header className="relative flex h-5 w-96 items-center justify-between px-6 py-6 pt-6">
-      <div className="flex items-center justify-center">
+    <header className="relative mb-6 flex h-5 w-96 items-center justify-between px-6 py-6 pt-6 lg:w-auto">
+      <div className="flex items-center justify-center lg:justify-between">
         <Image
           alt="Logotipo da Ecto"
           height={28}

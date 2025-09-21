ad69ce359bac391fa5ae517b046ac8b99f82cea0
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-09-21 16:56:00 -0300

    style(input): add responsive design for desktop screens

--- src/components/ui/form/input.tsx
+++ src/components/ui/form/input.tsx
@@ -19,7 +19,7 @@ export const Input = ({
   return (
     <input
       className={twMerge(
-        'h-10 w-[327px] items-center justify-start gap-2 rounded-md bg-white px-4 py-2 outline-1 outline-soft outline-offset-[-1px] placeholder:font-normal placeholder:text-base placeholder:text-sub placeholder:leading-normal hover:outline-sub" focus:outline-primary',
+        'h-10 w-[327px] items-center justify-start gap-2 rounded-md bg-white px-4 py-2 outline-1 outline-soft outline-offset-[-1px] placeholder:font-normal placeholder:text-base placeholder:text-sub placeholder:leading-normal hover:outline-sub" focus:outline-primary lg:w-[592px]',
         className
       )}
       id={name}

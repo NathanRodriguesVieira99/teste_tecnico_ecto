1af36946d4de129f5aadd77b2e91af42e5295e8d
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-09-21 16:55:12 -0300

    style(user-icon): add responsive design for desktop screens

--- src/components/ui/user-icon/user-icon.tsx
+++ src/components/ui/user-icon/user-icon.tsx
@@ -2,13 +2,15 @@ import Image from "next/image";
 
 export const UserIcon = () => {
   return (
-    <div className="items-start justify-start gap-4 rounded-[96px] bg-gradient-to-b from-zinc-200/50 via-stone-50/0 to-zinc-200/0 p-4 outline-1 outline-zinc-200 outline-offset-[-1px]">
-      <Image
-        alt="Ícone de usuário"
-        height={56}
-        src="/custom_icon.svg"
-        width={56}
-      />
+    <div className="lg:flex lg:w-full lg:items-start lg:justify-start">
+      <div className="items-start justify-start gap-4 rounded-[96px] bg-gradient-to-b from-zinc-200/50 via-stone-50/0 to-zinc-200/0 p-4 outline-1 outline-zinc-200 outline-offset-[-1px]">
+        <Image
+          alt="Ícone de usuário"
+          height={56}
+          src="/custom_icon.svg"
+          width={56}
+        />
+      </div>
     </div>
   );
 };

82dc283f14a67b0d296e44fa08f07bc625d1b20a
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-09-21 16:54:36 -0300

    style(layout): add responsive design for desktop screens

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Footer } from "@/components/layout/footer";
 import { Header } from "@/components/layout/header";
 
@@ -7,10 +8,24 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="h-full w-96">
-      <Header />
-      <main className="flex-1 bg-white">{children}</main>
-      <Footer />
+    <div className="flex">
+      <div className="mr-8 h-full w-96 overflow-hidden lg:w-[832px] lg:flex-col lg:py-12 lg:pl-28">
+        <Header />
+        <main className="flex-1 bg-white">{children}</main>
+        <Footer />
+      </div>
+
+      <div className="lg:p-4">
+        <div className="hidden lg:block lg:min-h-screen">
+          <Image
+            alt=""
+            className=""
+            height={992}
+            src="/Illustration.svg"
+            width={912}
+          />
+        </div>
+      </div>
     </div>
   );
 }

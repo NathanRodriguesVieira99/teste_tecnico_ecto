ce43aa0e0b9855cf21688cdb01ec54649cf7cf1a
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-09-21 16:56:59 -0300

    style(footer): add responsive design for desktop screens

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -4,24 +4,24 @@ import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <footer className="pt-10 w-96 gap-6 min-h-full  max-h-screen flex flex-col justify-start items-center ">
-      <div className="  inline-flex items-center justify-start gap-6">
+    <footer className="flex max-h-screen min-h-full w-96 flex-col items-center justify-start gap-6 pt-10 lg:w-auto lg:flex-row lg:justify-between">
+      <div className="flex items-center justify-start gap-6">
         {" "}
         <Link
+          className="font-semibold text-main text-sm leading-tight"
           href="#"
-          className="text-sm font-semibold  leading-tight text-main"
         >
           {" "}
           <span>Políticas de privacidade</span>
         </Link>
         <Link
+          className="font-semibold text-main text-sm leading-tight"
           href="#"
-          className="text-sm font-semibold leading-tight text-main"
         >
           <span>Termos de uso</span>
         </Link>
       </div>
-      <span className="text-sm font-semibold text-soft">
+      <span className="font-semibold text-sm text-soft">
         Teste de estágio {new Date().getFullYear()}
       </span>
     </footer>
